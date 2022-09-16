4645de959988538e042d82ab41f5580311bc32d9
venkatesh c <venky26494@yahoo.com>
2022-09-16 14:27:38 +0530

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,40 @@ module.exports = {
   content: [
     '*.html',
     '*/**.{html,css,js}',
+    './components/*.html'
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green-400': '#4ade80',
+        'blue-500': '#3b82f6',
+        'pink-500': '#ec4899',
+        'yellow-500': '#eab308',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
+    plugins: [],
+  }
 }
